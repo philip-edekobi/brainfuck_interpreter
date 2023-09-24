565185a44801a346697fb7858eb96304b0847b02
Philip Edekobi <philipedekobi@gmail.com>
2023-09-24 09:11:29 +0100

    loops working but nested loops failing

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-#define MEMSIZE 3000
+#define MEMSIZE 30000
+#define MAXLOOPDEPTH 1000
 
-int w_mem[MEMSIZE];
+int w_mem[MEMSIZE]; // working memory
 int ip; // instruction pointer
 
 int next_char(void);
@@ -68,7 +69,29 @@ void process_char(char ch) {
   }
 }
 
-void process_loop() {};
+void process_loop() {
+  int instructions[MAXLOOPDEPTH];
+  int curr_pos = 0;
+  int nest_level = 1;
+
+  while(nest_level) {
+    int next = next_char();
+    if (next == '[') {
+      nest_level++;
+    }
+    if (next == ']') {
+      nest_level--;
+      if(!nest_level) break;
+    }
+    instructions[curr_pos++] = next;
+  }
+
+  while(w_mem[ip]) {
+    for(int i = 0; i < curr_pos; i++) {
+      process_char(instructions[i]);
+    }
+  }
+};
 
 void end_loop() {};
 
